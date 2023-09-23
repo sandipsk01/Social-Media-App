1f2c98c08e054348df193ebcdae0f4fe2b3d8fda
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-09-23 23:03:15 +0530

    Working on Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import NavLinks from "./NavLinks";
+
+
+const Navbar = ()=>{
+    return (
+        <div className="flex justify-between items-center border-b border-gray-100 w-full px-44 py-2">
+            
+                <div className="text-3xl font-extrabold text-gray-900 dark:text-white font-roboto">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r to-red-600 from-blue-400">
+                        Social Media
+                    </span>{" "}
+                    App
+                </div>
+            
+            <div className="flex justify-center items-center mx-auto">
+                <NavLinks />
+            </div>
+            <div></div>
+        </div>
+    )
+};
+
+export default Navbar;
